e982e03c49374a9657a4eb9ae38afe5175ba3b38
CHBG27 <167037096+CHBG27@users.noreply.github.com>
2026-04-20 11:33:50 -0600

    consulta3 y 4

---
+++ SUBCONSULTAS.sql
@@ -0,0 +1,22 @@
+
+
+--Ejercicio 3
+-- Empleados que no son clientes
+SELECT FirstName + ' ' + LastName AS NombreCompleto
+FROM dbo.Employees
+EXCEPT
+SELECT ContactName
+FROM dbo.Customers
+ORDER BY NombreCompleto DESC;
+
+---Ejercicio 4
+SELECT e.EmployeeID, e.FirstName, e.LastName
+FROM dbo.Employees e
+WHERE e.EmployeeID NOT IN (
+    SELECT et.EmployeeID
+    FROM dbo.EmployeeTerritories et
+    INNER JOIN dbo.Territories t ON et.TerritoryID = t.TerritoryID
+    INNER JOIN dbo.Region r ON t.RegionID = r.RegionID
+    WHERE r.RegionDescription = 'Northern'
+)
+ORDER BY e.LastName DESC;
