541d4ff792bad77420cc53714f302e173947d602
jonajonagodinez08-dev <jonajonagodinez08@gmail.com>
2026-04-20 11:46:56 -0600

    consulta 8 y 9k

--- SUBCONSULTAS.sql
+++ SUBCONSULTAS.sql
@@ -23,6 +23,34 @@ WHERE e.EmployeeID NOT IN (
 )
 ORDER BY e.LastName DESC;
 >>>>>>> e982e03c49374a9657a4eb9ae38afe5175ba3b38
+<<<<<<< HEAD
+-- 8. Lista el nombre de los empleados que pertenezcan a la región Southern
+SELECT FirstName + ' ' + LastName AS NombreEmpleado
+FROM Employees
+WHERE EmployeeID IN (
+    SELECT EmployeeID 
+    FROM EmployeeTerritories 
+    WHERE TerritoryID IN (
+        SELECT TerritoryID 
+        FROM Territories 
+        WHERE RegionID = (
+            SELECT RegionID 
+            FROM Region 
+            WHERE RegionDescription = 'Southern'
+        )
+    )
+);
+
+-- 9. ID de la orden, fecha de pedido, fecha de entrega y flete de "United Package"
+SELECT OrderID, OrderDate, ShippedDate, Freight
+FROM Orders
+WHERE ShipVia = (
+    SELECT ShipperID 
+    FROM Shippers 
+    WHERE CompanyName = 'United Package'
+    
+);
+=======
 
 
 
@@ -32,3 +60,5 @@ ORDER BY e.LastName DESC;
 SELECT OrderID, CustomerID, ShipCountry
 FROM Orders
 WHERE ShipCountry IN ('Sweden', 'Spain');
+
+>>>>>>> 6aa1f32462a12e6d7c232f192fe5f02f1c2a4e8a
